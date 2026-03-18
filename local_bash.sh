bf696b9f61495861d1dddc89835d3ab1884f9e35
Dorin Doncenco <dorin.doncenco@cea.fr>
2026-03-18 14:44:15 +0100

    Updated local

--- local_bash.sh
+++ local_bash.sh
@@ -7,10 +7,16 @@ MODELPATH="/home/dorin/Research/repos/models/imagenet_cond_ddpm/256x256_diffusio
 SAMPLE_FLAGS="--batch_size 50 --timestep_respacing 200"
 # DATAPATH="/home/dorin/Research/repos/github/DiME_CelebA/CelebA/Img"
 DATAPATH="dataset_256.yml"
-CLASSIFIERPATH="DiME_Models/classifier.pth"
 ORACLEPATH="DiME_Models/oracle.pth"
-OUTPUT_PATH="output/002_AttemptNewDDPM"
-EXPNAME="DiffLoading"
+OUTPUT_PATH="output/004_AttemptNewDDPM"
+EXPNAME="004_Attempt_256_256"
+# CLASSIFIERPATH="DiME_Models/classifier.pth"
+# CABRNET=0
+CLASSIFIERPATH="DiME_Models/protopnet_resnet_256_cub.pth"
+CABRNET=1
+MODEL_ARCH="DiME_Models/model_arch.yml"
+TARGET_CLASS=37
+
 
 # parameters of the sampling
 GPU=0
@@ -27,6 +33,7 @@ IMAGESIZE=256  # dataset shape
 NUMBATCHES=50
 # CHUNK=0
 NUM_CHUNKS=20
+SUBSAMPLING=30
 
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate cabrnet
@@ -44,4 +51,6 @@ python -W ignore main.py $MODEL_FLAGS $SAMPLE_FLAGS \
     --l_perc $PERC --l_perc_layer $LAYER \
     --save_x_t True --save_z_t True \
     --use_sampling_on_x_t True \
-    --save_images True --image_size $IMAGESIZE --chunk 0 --num_chunks $NUM_CHUNKS
+    --save_images True --image_size $IMAGESIZE --chunk 0 --num_chunks $NUM_CHUNKS \
+    --subsampling $SUBSAMPLING --cabrnet $CABRNET --model_arch $MODEL_ARCH \
+    --target_class $TARGET_CLASS
